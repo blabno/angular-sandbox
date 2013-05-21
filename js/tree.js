1e2fbf840419779be9495862476da689e3152d5a
bernard <s4237@pjwstk.edu.pl>
2013-05-21 22:56:33 +0200

    Tree attempt

--- js/tree.js
+++ js/tree.js
@@ -4,6 +4,12 @@ itc.controller("TreeCtrl", function ($scope) {
     $scope.children = [
         {name:"A", children:[]}
     ];
+    $scope.selectedNode = $scope.children[0];
+    $scope.select = function (node) {
+        console.log(node)
+        $scope.selectedNode = node;
+    }
+
 });
 itc.controller("NodeCtrl", function ($scope) {
 
@@ -13,6 +19,11 @@ itc.controller("NodeCtrl", function ($scope) {
         node.children.push({name:new Date(), children:[]});
     };
 
+    $scope.alert = function (node) {
+        console.log("alert:" + node);
+        $scope.select({childa:node});
+    }
+
     $scope.toggle = function (node) {
         if (node.children.length == 0) {
             return;
@@ -20,4 +31,19 @@ itc.controller("NodeCtrl", function ($scope) {
         $scope.open = !$scope.open;
     };
 
+});
+
+itc.directive("treenode", function ($compile) {
+    return {
+        restrict:'E',
+        transclude:false,
+        scope:{value:'&', select:'&'},
+        templateUrl:'template/treeNode.html',
+        link:function (scope, elm, attrs, ngBindHtml) {
+            var childTemplate = '<treenode ng-repeat="child in value().children" value="child" select="select(childa)"/>';
+            var children = $compile(childTemplate)(scope);
+            elm.find("ul").append(children);
+        },
+        replace:false
+    }
 });
