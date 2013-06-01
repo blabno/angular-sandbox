5ab147607476becc9f909771efb5fb9d7d67e5b5
bernard <s4237@pjwstk.edu.pl>
2013-06-01 13:03:17 +0200

    New package and usecase buttons

--- js/tree.js
+++ js/tree.js
@@ -1,14 +1,15 @@
 var itc = angular.module("ITC", ["ui.bootstrap"]);
-
+var NODE_TYPE_PACKAGE = "package";
+var NODE_TYPE_USECASE = "usecase";
 itc.factory("packageDAO", function ()
 {
     /**
      * Mock data
      **/
     var rootNodes = [
-        {id: 1, name: "Requester", hasChildren: true},
-        {id: 2, name: "Supplier", hasChildren: false},
-        {id: 3, name: "Admin", hasChildren: false}
+        {id:1, name:"Requester", hasChildren:true},
+        {id:2, name:"Supplier", hasChildren:false},
+        {id:3, name:"Admin", hasChildren:false}
     ];
     var nodes = [
         rootNodes[0], rootNodes[1], rootNodes[2]
@@ -28,20 +29,30 @@ itc.factory("packageDAO", function ()
         parentNode.hasChildren = true;
     }
 
+    var node;
     for (var i = 5; i < 100; i++) {
-        var node = {id: i, name: "Node " + i, hasChildren: false};
+        node = {id:i, name:"Node " + i, hasChildren:false};
         var parentIndex = Math.floor(Math.random() * (nodes.length - 1)) + 1;
         /**We don't add children to Requester*/
         addChild(parentIndex, node);
     }
-    addChild(0, {id: i + 1, name: "Questionnaire"});
-    addChild(0, {id: i + 2, name: "Company"});
-    addChild(0, {id: i + 3, name: "Rating"});
+    addChild(0, {id:i + 1, name:"Questionnaire"});
+    addChild(0, {id:i + 2, name:"Company"});
+    addChild(0, {id:i + 3, name:"Rating"});
+    for (i = 0; i < nodes.length; i++) {
+        node = nodes[i];
+        var children = nodeChildMap[node.id];
+        if (undefined != children && children.length > 0) {
+            node.type = NODE_TYPE_PACKAGE;
+        } else {
+            node.type = NODE_TYPE_USECASE;
+        }
+    }
     /**
      * End of mock data
      */
     return {
-        list: function (parentId)
+        list:function (parentId)
         {
             console.debug("Lazy loading children of " + parentId);
             if (undefined == parentId) {
@@ -58,6 +69,9 @@ itc.factory("nodeFactory", function (packageDAO)
 {
     var create = function (pkg)
     {
+        if (NODE_TYPE_PACKAGE != pkg.type && NODE_TYPE_USECASE != pkg.type) {
+            throw new Error("Illegal type: " + pkg.type);
+        }
         function initChildren(node)
         {
             if (undefined == node.children) {
@@ -72,21 +86,21 @@ itc.factory("nodeFactory", function (packageDAO)
         }
 
         return {
-            type: "package", id: pkg.id, name: pkg.name, getChildren: function ()
+            type:pkg.type, id:pkg.id, name:pkg.name, getChildren:function ()
             {
                 if (!this.open) {
                     return [];
                 }
                 initChildren(this);
                 return this.children;
-            }, hasChildren: pkg.hasChildren, open: false, addChild: function (child)
+            }, hasChildren:pkg.hasChildren, open:false, addChild:function (child)
             {
                 initChildren(this);
                 this.children.push(child);
                 this.hasChildren = true;
                 this.open = true;
             },
-            removeChild: function (child)
+            removeChild:function (child)
             {
                 initChildren(this);
                 var index = this.children.indexOf(child);
@@ -97,23 +111,63 @@ itc.factory("nodeFactory", function (packageDAO)
         };
     };
     return {
-        create: create
+        create:create
     }
 });
 
 itc.controller("TreeCtrl", function ($scope, packageDAO, nodeFactory)
 {
-    $scope.child = nodeFactory.create({id: null, name: "Root", hasChildren: true});
+    $scope.child = nodeFactory.create({id:null, name:"Root", hasChildren:true, type:NODE_TYPE_PACKAGE});
     $scope.child.open = true;
 
     $scope.selectedNode = null;
     $scope.select = function (node)
     {
         $scope.selectedNode = node;
-    }
+    };
+
+    $scope.isSelected = function (node)
+    {
+        return $scope.selectedNode == node;
+    };
+
+    $scope.isPackage = function (node)
+    {
+        return null != node && NODE_TYPE_PACKAGE == node.type;
+    };
+
+    $scope.isUseCase = function (node)
+    {
+        return null != node && NODE_TYPE_USECASE == node.type;
+    };
+
+    $scope.remove = function (node)
+    {
+        return null != node && NODE_TYPE_PACKAGE == node.type;
+    };
+    $scope.newPackage = function ()
+    {
+        var name = prompt("Package name");
+        if (null != name && name.trim().length > 0) {
+            var parentNode = (null == $scope.selectedNode) ? $scope.child : $scope.selectedNode;
+            parentNode.addChild(nodeFactory.create({id:new Date().getTime(), name:name, hasChildren:false, type:NODE_TYPE_PACKAGE}));
+        }
+    };
+    $scope.newUsecase = function ()
+    {
+        if (null == $scope.selectedNode || !$scope.isPackage($scope.selectedNode)) {
+            alert("Select package");
+            return;
+        }
+        var name = prompt("Usecase name");
+        if (null != name && name.trim().length > 0) {
+            var parentNode = (null == $scope.selectedNode) ? $scope.child : $scope.selectedNode;
+            parentNode.addChild(nodeFactory.create({id:new Date().getTime(), name:name, hasChildren:false, type:NODE_TYPE_USECASE}));
+        }
+    };
 
 });
-itc.controller("NodeCtrl", function ($scope, nodeFactory)
+itc.controller("NodeCtrl", function ($scope)
 {
     function getNode()
     {
@@ -130,11 +184,6 @@ itc.controller("NodeCtrl", function ($scope, nodeFactory)
         return getNode().open;
     };
 
-    $scope.add = function ()
-    {
-        getNode().addChild(nodeFactory.create({id: new Date().getTime(), name: new Date(), hasChildren: false}));
-    };
-
     $scope.getChildren = function ()
     {
         return getNode().getChildren();
@@ -142,7 +191,11 @@ itc.controller("NodeCtrl", function ($scope, nodeFactory)
 
     $scope.removeMe = function ()
     {
-        getParentNode().removeChild(getNode());
+        var thisNode = getNode();
+        getParentNode().removeChild(thisNode);
+        if ($scope.selectedNode == thisNode) {
+            $scope.select(null);
+        }
     };
 
     $scope.toggle = function ()
@@ -159,16 +212,16 @@ itc.controller("NodeCtrl", function ($scope, nodeFactory)
 itc.directive("treenode", function ($compile)
 {
     return {
-        restrict: 'E',
-        transclude: false,
-        scope: false,
-        templateUrl: 'template/treeNode.html',
-        link: function (scope, elm)
+        restrict:'E',
+        transclude:false,
+        scope:false,
+        templateUrl:'template/treeNode.html',
+        link:function (scope, elm)
         {
             var childTemplate = '<li ng-repeat="child in getChildren()"><treenode></treenode></li>';
             var children = $compile(childTemplate)(scope);
             elm.find("ul").append(children);
         },
-        replace: true
+        replace:true
     }
 });
